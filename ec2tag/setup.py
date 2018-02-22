73bcc025998d74afaa81ea1693305c0c19f3117e
Joakim Uddholm <joakim@uddholm.com>
2018-02-22 11:46:21 +0100

    fix setup warts.

--- ec2tag/setup.py
+++ ec2tag/setup.py
@@ -3,13 +3,13 @@ import sys
 from setuptools import setup, find_packages
 
 setup(
-    name='ec2-by-tag',
+    name='ec2tag',
     version='0.0.1',
     author='Joakim Uddholm',
     author_email='tethik@gmail.com',
     description='Program to fetch ec2 instance private ips by their "Name" tag',
     url='https://github.com/Tethik/lame-cli-programs',
-    py_modules=['vaultauth'],
+    py_modules=['ec2tag'],
     entry_points = {
         'console_scripts': ['ec2-by-tag=ec2tag:main'],
     },
