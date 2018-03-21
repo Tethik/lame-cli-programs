a51b7abb18f6ab35147d42949a8b35dc509f0d5f
Joakim Uddholm <joakim@uddholm.com>
2018-03-21 19:27:27 +0100

    fix(*): skip instances not running.

--- ec2tag/ec2tag.py
+++ ec2tag/ec2tag.py
@@ -1,6 +1,8 @@
 import sys
 import boto3
 
+BOTO3_EC2_RUNNING_STATE = 16
+
 def main():
     if len(sys.argv) < 2:
         print(f'Usage: {sys.argv[0]} <tagname>')
@@ -9,6 +11,9 @@ def main():
     ec2 = boto3.resource('ec2')
 
     for i in ec2.instances.all():
+        if i.state["Code"] != BOTO3_EC2_RUNNING_STATE:
+            continue
+
         for tag in i.tags:
             if tag['Key'] == 'Name' and tag['Value'] == sys.argv[1]:
                 print(i.private_ip_address)
